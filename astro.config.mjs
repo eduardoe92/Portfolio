ab6e0edc16a3073166752ef46309d09baa41e9c0
eduardoe92 <eduardo.elias.1192@gmail.com>
2024-12-17 22:29:02 -0300

    act

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
-import { defineConfig } from 'astro/config'
-import tailwind from "@astrojs/tailwind"
-
-import robotsTxt from "astro-robots-txt"
+import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://eduardoeliaschacon-portfolio.vercel.app/',
   integrations: [tailwind(), robotsTxt()],
-  site: 'https://porfolio.dev/'
-})
+});
